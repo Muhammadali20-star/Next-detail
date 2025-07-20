a85b706fd68ffc0c2cd67970ad08f767271944cd
Muhammadali <muhamadalimamadaminov@gmail.com>
2025-07-20 08:32:58 +0300

    user

--- src/app/(laylout)/user/[id]/page.tsx
+++ src/app/(laylout)/user/[id]/page.tsx
@@ -1,12 +1,13 @@
 import UserDetails from '@/components/user-detail/UserDetails';
 import React from 'react'
 
-const UserDetailPage = async ({ params }: { params: { id: string } }) => {
-  const id = params.id; 
+const UserDetailPage = async (props: { params: Promise<{ id: string }> }) => {
+  const params = await props.params;
+  const id = params.id;
   const res = await fetch(`https://dummyjson.com/users/${id}`);
   const user = await res.json();
   console.log(user);
-  
+
 
   return (
     <div>
